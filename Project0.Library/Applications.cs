b1ab3de03f54d20bd1e6690b98b826cc70aaf4d4
keithkoltweit <keithkoltweit@gmail.com>
2020-11-10 19:02:43 -0700

    Added Interface and application class

---
+++ Project0.Library/Applications.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    public class Applications : IApplication
+    {
+        List<Customers> _customers = new List<Customers>();
+
+        public List<Customers> Customer
+        {
+            get
+            {
+                return _customers;
+            }
+
+            private set
+            {
+                _customers = value;
+            }
+
+        }
+         void IApplication.AddCustomer(Customers customer)
+        {
+            _customers.Add(customer);
+        }
+        
+    }
+}
---
+++ Project0.Library/IApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    public interface IApplication
+    {
+        void AddCustomer(Customers customer);
+        //void ViewCustomerHistory();
+        //void OrderHistory();
+        
+    }
+}
